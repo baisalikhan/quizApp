fe6aa081e0cc1ed016a01178768acb06fcd6cec6
Bais Ali Khan <112929397+baisalikhan@users.noreply.github.com>
2022-11-06 10:12:57 +0500

    first commit

---
+++ src/Helpers/QuestionBank.js
@@ -0,0 +1,34 @@
+export const Questions = [
+    {
+        prompt: "What is 1+3?",
+        optionA: "3",
+        optionB: "4",
+        optionC: "5",
+        optionD: "2",
+        answer: "B",
+    },
+    {
+        prompt: "What is 2+2?",
+        optionA: "5",
+        optionB: "4",
+        optionC: "3",
+        optionD: "6",
+        answer: "B",
+    },
+    {
+        prompt: "Which one is a Streaming Platform?",
+        optionA: "Twitter",
+        optionB: "Robinhood",
+        optionC: "Google",
+        optionD: "Twitch",
+        answer: "D",
+    },
+    {
+        prompt: "What is 2*2?",
+        optionA: "3",
+        optionB: "4",
+        optionC: "5",
+        optionD: "2",
+        answer: "B",
+    },
+]
